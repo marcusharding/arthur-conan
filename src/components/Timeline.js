b0a01c94ec1b80e1bc1bc53d014371a7b684bd47
marcusharding <marcus.harding01@googlemail.com>
2019-12-13 21:55:47 +0000

    Build out new slider | Slight config change

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,82 +1,60 @@
 import React from 'react';
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import TimelineTeaser from './02-teaser/TimelineTeaser';
 import Button from './03-objects/Button';
+import Slider from './02-teaser/Slider';
+
+const slideData = [
+    {
+      index: 0,
+      date: '1882',
+      headline: 'Conan Doyle leaves for Portsmouth',
+      src: 'https://picsum.photos/seed/picsum/1600/1600'
+    },
+    {
+      index: 1,
+      date: '1883',
+      headline: 'In The Wilderness',
+      src: 'https://picsum.photos/seed/picsum/1600/1600'
+    },
+    {
+      index: 2,
+      date: '1884',
+      headline: 'For Your Current Mood',
+      src: 'https://picsum.photos/seed/picsum/1600/1600'
+    },
+    {
+      index: 3,
+      date: '1885',
+      headline: 'Focus On The Writing',
+      src: 'https://picsum.photos/seed/picsum/1600/1600'
+    }
+  ]
+
+
 
 
 class Timeline extends React.Component {
-   render() {
-     const slides = [
-        {
-         img: 'https://picsum.photos/seed/picsum/1600/1600',
-         date: '1882',
-         subHeading: 'Lorem ipsum dolor sit amet'
 
-        },
-        {
-         img: 'https://picsum.photos/seed/picsum/1600/1600',
-         date: '1882',
-         subHeading: 'Lorem ipsum dolor sit amet'
-        },
-        {
-          img: 'https://picsum.photos/seed/picsum/1600/1600',
-          date: '1882',
-          subHeading: 'Lorem ipsum dolor sit amet'
-         },
-        {
-          img: 'https://picsum.photos/seed/picsum/1600/1600',
-          date: '1882',
-          subHeading: 'Lorem ipsum dolor sit amet'
-         },
-        {
-          img: 'https://picsum.photos/seed/picsum/1600/1600',
-          date: '1882',
-          subHeading: 'Lorem ipsum dolor sit amet'
-         },
-        {
-          img: 'https://picsum.photos/seed/picsum/1600/1600',
-          date: '1882',
-          subHeading: 'Lorem ipsum dolor sit amet'
-         },
-      ];
-     var settings = {
-       dots: true,
-       infinite: false,
-       centerMode: true,
-       centerPadding: '20%',
-       speed: 500,
-       slidesToShow: 1,
-       slidesToScroll: 1,
-       arrows: false
-     };
+   render() {
+     
      return (
-      <section className="pb-12 h-screen">
-        <div className="wrapper">
-          <p className="text-white text-3xl py-12">Timeline</p>
-        </div>
-        <div className="">
-          <Slider {...settings}>
-              {slides.map((props) => {
-                return (
-                    <div className="pr-12">
-                      <TimelineTeaser
-                        img={props.img}
-                        date={props.date}
-                        subHeading={props.subHeading}
-                        />
-                    </div>
-                )
-              })}
-          </Slider>
-        </div>
-        <div className="wrapper text-center py-12">
-          <Button
-            btnText={'Go Back'}
-           />
-        </div>
-      </section>
+        <React.Fragment>
+            <section className="h-auto overflow-x-hidden">
+                <div className="wrapper">
+                    <p className="text-white text-3xl py-4">Timeline</p>
+                </div>
+            <div className="">
+                <Slider
+                heading="Example Slider"
+                slides={slideData}
+                />
+            </div>
+            <div className="wrapper text-center pt-20 relative">
+                <Button
+                    btnText={'Go Back'}
+                />
+            </div>
+        </section>
+      </React.Fragment>
      );
    }
  }
---
+++ src/components/Timeline_old.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import TimelineTeaser from './02-teaser/TimelineTeaser';
+import Button from './03-objects/Button';
+
+
+class Timeline extends React.Component {
+   render() {
+     const slides = [
+        {
+         img: 'https://picsum.photos/seed/picsum/1600/1600',
+         date: '1882',
+         subHeading: 'Lorem ipsum dolor sit amet'
+
+        },
+        {
+         img: 'https://picsum.photos/seed/picsum/1600/1600',
+         date: '1882',
+         subHeading: 'Lorem ipsum dolor sit amet'
+        },
+        {
+          img: 'https://picsum.photos/seed/picsum/1600/1600',
+          date: '1882',
+          subHeading: 'Lorem ipsum dolor sit amet'
+         },
+        {
+          img: 'https://picsum.photos/seed/picsum/1600/1600',
+          date: '1882',
+          subHeading: 'Lorem ipsum dolor sit amet'
+         },
+        {
+          img: 'https://picsum.photos/seed/picsum/1600/1600',
+          date: '1882',
+          subHeading: 'Lorem ipsum dolor sit amet'
+         },
+        {
+          img: 'https://picsum.photos/seed/picsum/1600/1600',
+          date: '1882',
+          subHeading: 'Lorem ipsum dolor sit amet'
+         },
+      ];
+     var settings = {
+       dots: true,
+       infinite: false,
+       centerMode: true,
+       centerPadding: '20%',
+       speed: 500,
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       arrows: false
+     };
+     return (
+      <section className="pb-12 h-screen">
+        <div className="wrapper">
+          <p className="text-white text-3xl py-12">Timeline</p>
+        </div>
+        <div className="">
+          <Slider {...settings}>
+              {slides.map((props) => {
+                return (
+                    <div className="pr-12">
+                      <TimelineTeaser
+                        img={props.img}
+                        date={props.date}
+                        subHeading={props.subHeading}
+                        />
+                    </div>
+                )
+              })}
+          </Slider>
+        </div>
+        <div className="wrapper text-center py-12">
+          <Button
+            btnText={'Go Back'}
+           />
+        </div>
+      </section>
+     );
+   }
+ }
+ 
+export default Timeline;
