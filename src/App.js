39add8640e2911f5e09931b1cae5666cbf53eec6
marcusharding <marcus.harding01@googlemail.com>
2020-02-10 18:12:21 +0000

    sorting out react-router issues

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './Home';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
+import { PortsmouthMap } from './components/Map';
+import { Timeline } from './components/04-components/Timeline';
+import Error from './components/Error';
 
 export class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
+      <Fragment>
+        <BrowserRouter>
+          <div>
+              <Switch>
+              <Route path="/" component={LandingPage} exact/>
+              <Route path="/Map" component={PortsmouthMap}/>
+              <Route path="/Timeline" component={Timeline}/>
+              <Route component={Error}/>
+            </Switch>
+          </div> 
+        </BrowserRouter>
+      </Fragment>
     );
   }
 }
