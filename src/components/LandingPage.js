1d627deb8f6be90cca8b3e89b8514c54b2437e53
marcusharding <marcus.harding01@googlemail.com>
2020-02-26 22:37:11 +0000

    Tidy up some homepage styling

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -29,11 +29,11 @@ export class LandingPage extends React.Component {
         <div className="header">
           <img alt="" className="header_img opacity-25" src={BgImage} />
           <div className="z-10 text-center wrapper">
-            <h2 className="font-gabriela text-white lg:text-4xl">The life of</h2>
-            <h1 className="font-gabriela text-white lg:text-6xl pb-6">Arthur Conan-Doyle</h1>
-            <p className="text-white lg:text-2xl">The best way to navigate the life of Arthur Conan-Doyle, lets get started</p>
+            <h2 className="font-gabriela text-white text-2xl lg:text-4xl">The life of</h2>
+            <h1 className="font-gabriela text-white text-3xl lg:text-6xl pb-6">Arthur Conan-Doyle</h1>
+            <p className="text-white lg:text-2xl pb-6 lg:pb-12">The best way to navigate the life of Arthur Conan-Doyle, lets get started</p>
             <div className="home-buttons__container flex flex-col lg:flex-row justify-center items-center">
-              <NavLink className="btn btn--primary lg:mr-8" to="/Map">Map</NavLink>
+              <NavLink className="btn btn--primary mb-4 lg:mb-0 lg:mr-8" to="/Map">Map</NavLink>
               <NavLink className="btn btn--secondary" to="/Timeline">Timeline</NavLink>
             </div>
           </div>
