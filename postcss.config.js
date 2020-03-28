b2480f1cd9e9f325d64224b0c5ae18becce42aeb
marcusharding <marcus.harding01@googlemail.com>
2020-03-28 15:29:01 +0000

    Last test of postcss glob

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: [
-    './src/components/**/*.jsx',
-    './src/components/**/*.js',
+    './src/components/**/**/**/*.jsx',
+    './src/components/**/**/**/*.js',
     './public/index.html'
   ],
   css: ['./src/tailwind.css'],
