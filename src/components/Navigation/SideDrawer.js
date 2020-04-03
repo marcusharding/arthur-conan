f5e8bc8b14d1f058330a571b84ac60c03c62cab9
Luke-Desktop <up858296@myport.ac.uk>
2020-04-03 18:42:29 +0100

    removed drawer close button, highlighted current page in drawer and added logo

--- src/components/Navigation/SideDrawer.js
+++ src/components/Navigation/SideDrawer.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import SideDrawerButton from './SideDrawerButton'
+import TimelineIcon from '@material-ui/icons/Timeline';
+import ExploreIcon from '@material-ui/icons/Explore';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import ACD_logo from '../../assets/images/logo512.png';
 
 const sideDrawer = props => {
     let drawerClasses = 'side-drawer';
@@ -10,11 +13,12 @@ const sideDrawer = props => {
     return (
         <nav className={drawerClasses}>
             <div>
-                <SideDrawerButton click={props.sidedrawerClickHandler} />
+                <img alt='' className='logo' src={ACD_logo} />
             </div>
             <ul>
-            <NavLink onClick={props.sidedrawerClickHandler} to='/Map'><li>Map</li></NavLink>
-            <NavLink onClick={props.sidedrawerClickHandler} to='/Timeline'><li>Timeline</li></NavLink>
+                <NavLink onClick={props.sidedrawerClickHandler} to='/Map' activeClassName='current' ><li><ExploreIcon className='Nav_icons' />Map</li></NavLink>
+                <NavLink onClick={props.sidedrawerClickHandler} to='/Timeline' activeClassName='current' ><li><TimelineIcon className='Nav_icons' />Timeline</li></NavLink>
+                <NavLink onClick={props.sidedrawerClickHandler} to='/About' activeClassName='current' ><li><SupervisorAccountIcon className='Nav_icons' />About</li></NavLink>
             </ul>
         </nav>
     );
