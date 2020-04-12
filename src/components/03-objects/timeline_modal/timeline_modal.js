2377d3dd5e3cb804bedddfa1be1ac09422011fb8
marcusharding <marcus.harding01@googlemail.com>
2020-04-12 22:24:42 +0100

    Remove modal excerpt from mobile for now

--- src/components/03-objects/timeline_modal/timeline_modal.js
+++ src/components/03-objects/timeline_modal/timeline_modal.js
@@ -7,12 +7,10 @@ export class TimelineModal extends React.Component {
     render() {
         return(
             <React.Fragment>
-            <div className="arthurs-house-txt--container">
-                <div className={`absolute w-1/3 h-full z-50 left-0 text-white items-center flex-col justify-center text-center ${this.props.slides.arthursHouse === true && this.props.modalActive === true ? 'flex' : 'hidden'} ${this.props.slides.index === this.props.current ? 'visible' : 'invisible'}`}>
+                <div className={`absolute w-1/3 h-full z-50 left-0 text-white items-center flex-col justify-center text-center hidden lg:${this.props.slides.arthursHouse === true && this.props.modalActive === true ? 'flex' : 'hidden'} ${this.props.slides.index === this.props.current ? 'visible' : 'invisible'}`}>
                 <p className="text-3xl pb-4">{this.props.slides.location}</p>
                 <p className="w-1/2 text-lg">{this.props.slides.caption}</p>
                 </div>
-            </div>
             <div data-key={this.props.slides.index} className={`timeline-Modal__container bg-white ${this.props.modalActive === false ? 'hidden' :  'block'} ${this.props.slides.index === this.props.current ? 'visible' : 'invisible'}`}>
                 <div onClick={() => {this.props.modalPrevious(this.props.slides);}} className={`modal-btn--previous ${this.props.slides.arthursHouse === false ? 'hidden' : 'flex'}`}><img alt="" className="text-white w-1/3" src={backArrow} /></div>
                 <div onClick={() => {this.props.modalNext(this.props.slides);}} className={`modal-btn--next ${this.props.slides.arthursHouse === false ? 'hidden' : 'flex'}`}><img alt="" className="text-white w-1/3" src={nextArrow} /></div>
