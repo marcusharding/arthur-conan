94069d2b21c4340f91921bbb96019af7888576b5
marcusharding <marcus.harding01@googlemail.com>
2020-05-02 23:00:52 +0100

    Add 100vh module to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import BgImage from '../assets/images/landing_background.jpg';
+import Div100vh from 'react-div-100vh'
 
 export class LandingPage extends React.Component {
 
@@ -22,19 +23,20 @@ export class LandingPage extends React.Component {
   render() {
     return (
       <React.Fragment>
-        {/* Loading component  */}
-        <div className="header">
-          <img alt="Landing Page Background" className="header_img opacity-25"  src={BgImage} />
-          <div className="z-10 text-center wrapper">
-            <h2 className="font-gabriela text-white text-2xl lg:text-4xl">The life of</h2>
-            <h1 className="font-gabriela text-white text-3xl lg:text-6xl pb-6">Arthur Conan Doyle</h1>
-            <p className="text-white lg:text-2xl pb-6 lg:pb-12">The best way to navigate the life of Arthur Conan Doyle, lets get started</p>
-            <div className="home-buttons__container flex flex-col lg:flex-row justify-center items-center">
-              <NavLink className="btn btn--primary mb-4 lg:mb-0 lg:mr-8 uppercase" to="/Map">Map</NavLink>
-              <NavLink className="btn btn--secondary uppercase" to="/Timeline">Timeline</NavLink>
+        <Div100vh>
+          <div className="h-full flex items-center">
+            <img alt="Landing Page Background" className="header_img opacity-25"  src={BgImage} />
+            <div className="z-10 text-center wrapper">
+              <h2 className="font-gabriela text-white text-2xl lg:text-4xl">The life of</h2>
+              <h1 className="font-gabriela text-white text-3xl lg:text-6xl pb-6">Arthur Conan Doyle</h1>
+              <p className="text-white lg:text-2xl pb-6 lg:pb-12">The best way to navigate the life of Arthur Conan Doyle, lets get started</p>
+              <div className="home-buttons__container flex flex-col lg:flex-row justify-center items-center">
+                <NavLink className="btn btn--primary mb-4 lg:mb-0 lg:mr-8 uppercase" to="/Map">Map</NavLink>
+                <NavLink className="btn btn--secondary uppercase" to="/Timeline">Timeline</NavLink>
+              </div>
             </div>
           </div>
-        </div>
+        </Div100vh>
       </React.Fragment>
     )
   }
